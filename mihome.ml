393d0e3919d57151a07239dd644e434a62c6a87d
jaiverma <jai2.verma@outlook.com>
2021-06-12 16:24:22 +0530

    Refactoring...

--- mihome.ml
+++ mihome.ml
@@ -9,7 +9,42 @@ type mihome = {
   } [@@big_endian]
 ]
 
+let mi_mac = "52:ec:50:83:5f:64"
+
+let token_key = ref Cstruct.empty
+
 let validate_packet packet =
     match get_mihome_magic packet with
     | 0x2131 -> Ok ()
     | _ -> Error "invalid magic"
+
+
+let update_token mac token =
+    match (Cstruct.is_empty !token_key) with
+    | true -> (
+        match (String.compare mac mi_mac) with
+        | 0 -> (
+            match (Cstruct.get_byte token 0) with
+            | 0xff -> ()
+            | _ -> token_key := token)
+        | _ -> ())
+    | false -> ()
+
+let decrypt_payload payload token =
+    let open Nocrypto.Cipher_block in
+    let open Nocrypto.Hash in
+
+    let key = token |> MD5.digest in
+    let iv =
+        Cstruct.append key token
+        |> MD5.digest
+    in
+    let plain =
+        AES.CBC.decrypt
+            ~key:(AES.CBC.of_secret key)
+            ~iv
+            payload
+        |> Utils.unpad_pkcs7
+    in
+
+    plain
--- pcap_parse.ml
+++ pcap_parse.ml
@@ -1,15 +1,6 @@
 module Def = Definitions
 module Mi = Mihome
 
-let glob_token = "\x18\x6d\xaf\x33\x67\xcc\x57\x3f\x3f\x6a\x7e\x09\xfb\x8e\x0c\xf4"
-    |> Cstruct.of_string
-
-type direction = INCOMING | OUTGOING
-
-let mi_mac = "52:ec:50:83:5f:64"
-
-let token_key = ref Cstruct.empty
-
 let open_file filename =
     let fd = Unix.openfile filename [O_RDONLY] 0 in
     Mmap.V1.map_file fd Bigarray.char Bigarray.c_layout false [|-1|]
@@ -30,7 +21,7 @@ let read_packets filename =
 
     let rec parse iterator =
         match (iterator ()) with
-        | Some (_hdr, eth) ->
+        | Some (_hdr, eth) -> (
             let mac_src = Def.get_mac_src eth in
             let mac_dst = Def.get_mac_dst eth in
             let ip = Cstruct.shift eth Def.sizeof_ethernet in
@@ -39,60 +30,32 @@ let read_packets filename =
             let udp = Cstruct.shift ip Def.sizeof_ipv4 in
             let mi_data = Cstruct.shift udp Def.sizeof_udpv4 in
 
-            Printf.printf "mac src: %s\n" mac_src;
-            Printf.printf "mac dst: %s\n" mac_dst;
-            Printf.printf "ip src: %s\n" ip_src;
-            Printf.printf "ip dst: %s\n" ip_dst;
-
             Result.bind (Mi.validate_packet mi_data)
             (fun _ ->
             let mi_len = Mi.get_mihome_len mi_data in
-            Printf.printf "[+] packet len: %d\n" mi_len;
-            Printf.printf "\t[!] payload len: %d\n" (mi_len - Mi.sizeof_mihome);
+            let token = Mi.get_mihome_token mi_data in
+            Mi.update_token mac_src token;
 
-            let token = mi_data |> Mi.get_mihome_token
+            let payload =
+                match mi_len with
+                | x when x = Mi.sizeof_mihome -> Cstruct.empty
+                | _ -> (
+                    let payload = Cstruct.shift mi_data Mi.sizeof_mihome in
+                    Mi.decrypt_payload payload !Mi.token_key)
             in
-            token |> Cstruct.to_bytes ~len:16 |> Utils.print_byte_data;
-            Printf.printf "----------------\n";
 
-            (match (Cstruct.is_empty !token_key) with
-            | true -> (
-                match (String.compare mac_src mi_mac) with
-                | 0 -> (
-                    match (Cstruct.get_byte token 0) with
-                    | 0xff -> ()
-                    | _ -> token_key := token)
-                | _ -> ())
-            | false -> ());
+            Printf.printf "src: [%s] [%s] -> dst: [%s] [%s]\n"
+                mac_src ip_src mac_dst ip_dst;
+            (match (Cstruct.length payload) with
+            | 0 -> ()
+            | _ -> Printf.printf "%s\n" @@ Cstruct.to_string payload);
 
-            if mi_len = Mi.sizeof_mihome then
-                Printf.printf "no extra data\n"
-            else (
-                Printf.printf "extra data present\n";
-                let open Nocrypto.Cipher_block in
-                let open Nocrypto.Hash in
-
-                let payload = Cstruct.shift mi_data Mi.sizeof_mihome in
-                let key = !token_key |> MD5.digest in
-                let iv =
-                    Cstruct.append key !token_key
-                    |> MD5.digest
-                in
-                let plain =
-                    AES.CBC.decrypt
-                        ~key:(AES.CBC.of_secret key)
-                        ~iv
-                        payload
-                    |> Utils.unpad_pkcs7
-                in
-
-                Cstruct.to_string plain |> Printf.printf "%s\n"
-            );
-            parse iterator)
+            parse iterator))
 
         | None -> Error "no packets left"
     in
+
     parse data_iter
 
-let _ =
-    read_packets "/tmp/yolo.pcap"
+let () =
+    ignore @@ read_packets "/tmp/yolo.pcap"
