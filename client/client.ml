ca492052828827735a08bbfdbaf6d4cf343e82a2
jaiverma <jai2.verma@outlook.com>
2021-06-13 18:56:13 +0530

    Refactoring UDP client

--- client/client.ml
+++ client/client.ml
@@ -12,28 +12,59 @@ let send_recv_data buf _r w =
     Writer.write_bytes w buf;
     ignore @@ Writer.flushed w
 
-let run ~addr ~port data =
+let manage_socks ~addr ~port f =
     let sock = bind_any () in
     Monitor.protect
         ~run:`Schedule
         ~finally:(fun () -> Fd.close @@ Socket.fd sock)
         (fun () ->
             let `Inet (_host, _port) = Unix.Socket.getsockname sock in
-            let addr = Socket.Address.Inet.create ~port @@ Unix.Inet_addr.of_string addr in
-            let send_fn = Or_error.ok_exn @@ sendto () in
-            send_fn (Socket.fd sock) (Iobuf.of_bytes data) addr
-            >>= (fun _ ->
-            recvfrom_loop (Socket.fd sock) (fun b _ -> Iobuf.to_string b |> my_log))
-            >>| (fun res ->
-            match res with
-            | Closed -> my_log "socket closed!\n"
-            | _ -> my_log "something else happend\n"
-            ))
-            (* let ret = sendto (Socket.fd sock) (Iobuf.of_string "hello world\n") addr in
-            match ret with
-            | Ok
-            | Error -> ()) *)
+            let addr =
+                Socket.Address.Inet.create ~port
+                @@ Unix.Inet_addr.of_string addr
+            in
+            f ~sock ~addr)
 
+let send_recv_one ~addr ~port payload =
+    match sendto () with
+    | Error _ -> return ()
+    | Ok send_fn ->
+        manage_socks
+            ~addr
+            ~port
+            (fun ~sock ~addr ->
+                let stopped = ref false in
+                let recvd = Bvar.create () in
+
+                if !stopped then return ()
+                else Deferred.all_unit [
+                    Deferred.all_unit [
+                        send_fn
+                            (Socket.fd sock)
+                            (Iobuf.of_bytes payload)
+                            addr;
+                        Bvar.wait recvd
+                    ];
+
+                    Monitor.try_with
+                        ~run:`Schedule
+                        ~rest:`Log
+                        (fun () ->
+                            recvfrom_loop
+                                (Socket.fd sock)
+                                (fun b _ ->
+                                    let buf = Iobuf.to_string b in
+                                    my_log buf;
+                                    Bvar.broadcast recvd ();
+                                    stopped := true;
+
+                                    ignore
+                                    @@ Fd.close
+                                    @@ Socket.fd sock))
+                    >>| function
+                    | Ok (Closed | Stopped) -> ()
+                    | Error e -> raise e
+                ])
 
 let () =
     let _packet = Mi.create_packet
@@ -43,7 +74,10 @@ let () =
         ~token:"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
     in
 
-    ignore @@ run ~addr:"127.0.0.1" ~port:54321
-        @@ Bytes.of_string "hello world\n";
+    send_recv_one
+        ~addr:"127.0.0.1"
+        ~port:54321
+        @@ Bytes.of_string "hello world\n"
+    |> ignore;
 
     never_returns @@ Scheduler.go ()
